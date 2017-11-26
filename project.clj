cdcb5e0a2675d386679ccb6aa83d3dc9b6e1971a
David Goldfarb <deg@degel.com>
2017-11-26 16:07:44 +0200

    Whoops, fixed URL. v0.1.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject
-  com.degel/iron "0.1.0"
+  com.degel/iron "0.1.1"
   :description "Front-end utilities and support for ClojureScript and re-frame projects"
-  :url "https://github.com/deg/sodium"
+  :url "https://github.com/deg/iron"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-RC1"]
