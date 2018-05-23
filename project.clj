5c91a1dc4b7e9c88669c7ccca94868fbc6490fc1
David Goldfarb <deg@degel.com>
2018-05-23 12:47:16 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
-                 [expound "0.5.0"]
+                 [expound "0.6.0"]
                  [re-frame "0.10.5"]
-                 [reagent "0.7.0"]]
+                 [reagent "0.8.1"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.8"]]
   :cljsbuild
